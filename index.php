d2c82bb52da5ce01e3778646d3af30b21eefc0ff
vietduc-cutephomaique <75103752+vietduc-cutephomaique@users.noreply.github.com>
2020-12-02 09:35:52 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bài tập giới thiệu bản thân</title>
+	<H1>Giới thiệu Badboy Aptech </H1>
+	<H2>Full name : Trinh VIet Duc </H2>
+	<H3>Age : 18 </H3>
+	<H3>Sex: male</H3>
+	<H3>Address : 1 Phung Khac Khoan Stress</H3>
+	<H3>Address e-mail : trinhvietduc.06012002@gmail.com</H3>
+	<H3>Number phone : 0965505542</H3>
+	<H3>Character : Hòa Đồng , Vui Vẻ , Hài Hước , Đep trai , blablo...</H3>
+	<img src="https://scontent.fhan2-2.fna.fbcdn.net/v/t1.0-9/96790667_264139541394908_921966712075583488_n.jpg?_nc_cat=111&ccb=2&_nc_sid=174925&_nc_ohc=l9VtlWah4asAX9k2jiV&_nc_oc=AQk83bTzRRdmXiNVK51qLbeDUk2ufC229YwxeWkN4wBYaCyJFaFr7hRNhD3TSjEaxio&_nc_ht=scontent.fhan2-2.fna&oh=4611b1b83eeb7aa70e89c638e4e2555a&oe=5FEB0E8A">
+
+<body>
+</body>
+</html>
